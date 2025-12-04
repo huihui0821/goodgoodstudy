b4e8f9ed1eb38e3a180a29ee25875d9e32d5eca0
huihui0821 <71921374+huihui0821@users.noreply.github.com>
2025-12-04 23:06:24 +0800

    回文数字简单解法

--- singleNumber/singleNum2.go
+++ singleNumber/singleNum2.go
@@ -1,1 +1,19 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	num := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 5, 4, 3, 2, 1}
+	fmt.Printf("singleNumber(num[:]): %v\n", singleNumber(num[:]))
+
+}
+
+func singleNumber(nums []int) int {
+	single := 0
+	for _, nummber := range nums {
+		single ^= nummber
+	}
+	return single
+}
