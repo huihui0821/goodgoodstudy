b2fbcf2b9e29bef9725f21b78f68980f2b2038c9
huihui0821 <872860375@qq.com>
2025-12-06 22:41:24 +0800

    1

---
+++ mergeIntervals/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	arrs := [][]int{{2, 4}, {1, 3}, {5, 7}, {8, 10}}
+	fmt.Printf("arrs: %v\n", arrs)
+	ret := mergeIntervals(arrs)
+	fmt.Printf("ret: %v\n", ret)
+}
+
+func mergeIntervals(arrs [][]int) [][]int {
+	lenArr := len(arrs)
+	if lenArr <= 1 {
+		return arrs
+	}
+
+	retArrs := make([][]int, 0, lenArr)
+	// 使用sort库函数对二维数字进行排序
+	sort.Slice(arrs, func(i, j int) bool {
+		return arrs[i][0] < arrs[j][0]
+	})
+	fmt.Printf("arrs: %v\n", arrs)
+
+	retArrs = append(retArrs, arrs[0])
+	for i := 1; i < lenArr; i++ {
+		arr := arrs[i]
+		ret := retArrs[(len(retArrs))-1]
+		// 出现区间重叠
+		if ret[1] >= arr[0] {
+			retArrs[len(retArrs)-1][1] = arr[1]
+		} else {
+			// m没有重叠就将arrs[i] 追加到retArrs,添加一个新的区间
+			retArrs = append(retArrs, arrs[i])
+		}
+	}
+
+	return retArrs
+}
